ba01181de496110e95766fb9e59bb4f7164bd277
mhhaekal <mhaekal55@gmail.com>
2023-10-13 22:37:54 +0700

    Clean App.js FE
    Haekal-Done

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,34 +1,8 @@
-
 //import component/pages
-import Navbar from "./components/navbarUser";
-import NavbarAdmin from "./components/navbarAdmin";
-import Footer from "./components/footer";
-import Button from "./components/button";
-import ProductCard from "./components/productCard";
 
 function App() {
   return (
     <div data-theme="light">
-
-      <Navbar />
-      {/* <NavbarAdmin /> */}
-      <div className="mt-[70px] flex flex-col justify-evenly p-5 lg:px-32 border">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-10 place-items-center">
-          <ProductCard name={"Buah Alpukat adasdadasd"} />
-          <ProductCard name={"Kiwi Hijau"} />
-          <ProductCard name={"Apel Fuji Premium"} />
-          <ProductCard name={"Blueberry Impor"} />
-          <ProductCard name={"Jeruk Mandarin Murcott"} />
-          <ProductCard name={"Kiwi Hijau"} />
-          <ProductCard name={"Kiwi Hijau"} />
-          <ProductCard name={"Kiwi Hijau"} />
-        </div>
-
-        <div className="mt-10"><Button text={"Confirm"} style={"w-[200px]"} /></div>
-
-
-      </div>
-      <Footer />
 
     </div>
   );
